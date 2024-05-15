519ea1b955275a15f08570e8f314f6f15f3ade93
Trina-Dasgupta-Official <trinadasgupta98@gmail.com>
2024-05-15 19:32:56 +0530

    backend added and initialized code

--- foodmine/frontend/src/app/app.module.ts
+++ foodmine/frontend/src/app/app.module.ts
@@ -9,6 +9,9 @@ import { StarRatingComponent } from './components/partials/star-rating/star-rati
 import { SearchComponent } from './components/partials/search/search.component';
 import { TagsComponent } from './components/partials/tags/tags.component';
 import { FoodPageComponent } from './components/pages/food-page/food-page.component';
+import { CartPageComponent } from './components/pages/cart-page/cart-page.component';
+import { TitleComponent } from './components/partials/title/title.component';
+import { NotFoundComponent } from './components/partials/not-found/not-found.component';
 
 
 @NgModule({
@@ -19,7 +22,10 @@ import { FoodPageComponent } from './components/pages/food-page/food-page.compon
     StarRatingComponent,
     SearchComponent,
     TagsComponent,
-    FoodPageComponent
+    FoodPageComponent,
+    CartPageComponent,
+    TitleComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
