d425593e9c563de164d18c53ac95ccc1ac99ef6b
Trina Dasgupta <88958773+Trina-Dasgupta@users.noreply.github.com>
2024-06-12 18:31:37 +0530

    Update database.config.ts

--- foodmine/backend/src/configs/database.config.ts
+++ foodmine/backend/src/configs/database.config.ts
@@ -2,8 +2,7 @@ import {connect,ConnectOptions} from 'mongoose';
 
 export const dbConnect=()=>{
     connect(process.env.MONGO_URI!,{
-        useNewUrlParser:true,
-    } as ConnectOptions).then(()=>{
+       useUnifiedTopology: true, } as ConnectOptions).then(()=>{
         console.log("Connected successfully");
         
     },error=>{
