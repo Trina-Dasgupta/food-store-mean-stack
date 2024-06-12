767f881ad2dc8ddf12df563a9ad3e7bb7ba0c460
Trina Dasgupta <88958773+Trina-Dasgupta@users.noreply.github.com>
2024-06-12 20:42:00 +0530

    Update server.ts

--- foodmine/backend/src/server.ts
+++ foodmine/backend/src/server.ts
@@ -20,10 +20,10 @@ app.use("/api/foods", foodRouter);
  app.use("/api/users", userRouter);
  app.use("/api/orders", orderRouter);
 
-// app.use(express.static('public'));
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname,'public', 'index.html'))
-// })
+app.use(express.static('public'));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname,'public', 'index.html'))
+})
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
